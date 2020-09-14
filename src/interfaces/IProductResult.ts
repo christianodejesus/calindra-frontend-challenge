fefd9371843126f98735b899bea986cb5bb324c8
Christiano Marques <8249890+christianodejesus@users.noreply.github.com>
2020-09-14 02:09:19 -0300

    App commit

---
+++ src/interfaces/IProductResult.ts
@@ -0,0 +1,13 @@
+export interface IProductResult {
+    map: {
+        defaultPrice: Array<number>,
+        "images.url": Array<string>,
+        "images.id": Array<number>,
+        salePrice: Array<number>,
+        name: Array<string>,
+        "images.type": Array<string>,
+        id: Array<number>,
+        "images.position": Array<number>,
+        "uri": Array<string>
+    }
+}
